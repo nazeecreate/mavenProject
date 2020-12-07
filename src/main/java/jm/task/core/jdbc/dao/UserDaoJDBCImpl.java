bc2f0d514ab1d8f4ef977876e4f8788b1b667c7e
Nazee <is8universe@gmail.com>
2020-12-07 21:43:43 +0600

    Task edited

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,8 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,76 +12,120 @@ public class UserDaoJDBCImpl implements UserDao {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=UTC&useSSL=false";
     private static final String USERNAME = "nazee";
     private static final String PASSWORD = "Antanta22!";
+    private Util util = new Util();
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             Statement statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS  mydbtest.users (\n" +
-                    "  id INT NOT NULL AUTO_INCREMENT,\n" +
-                    "  name VARCHAR(45) NOT NULL,\n" +
-                    "  lastname VARCHAR(45) NOT NULL,\n" +
-                    "  age INT(3) NOT NULL,\n" +
-                    "  PRIMARY KEY (id));");
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        String sql = "CREATE TABLE IF NOT EXISTS  mydbtest.users (\n" +
+                "  id INT NOT NULL AUTO_INCREMENT,\n" +
+                "  name VARCHAR(45) NOT NULL,\n" +
+                "  lastname VARCHAR(45) NOT NULL,\n" +
+                "  age INT(3) NOT NULL,\n" +
+                "  PRIMARY KEY (id));";
+        runSql(sql);
     }
 
     public void dropUsersTable() {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             Statement statement = connection.createStatement()) {
-            statement.execute("DROP TABLE IF EXISTS users;");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        String sql = "DROP TABLE IF EXISTS users";
+        runSql(sql);
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate(String.format("INSERT INTO users (name, lastname, age) values ('%s', '%s', %s);", name, lastName, age));
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        String sql = String.format("INSERT INTO users (name, lastname, age) values ('%s', '%s', %s);", name, lastName, age);
+        runSql(sql);
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate(String.format("DELETE FROM users WHERE id=%s;", id));
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    public List<User> getAllUsers() {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             Statement statement = connection.createStatement()) {
-            List<User> list = new LinkedList<>();
-            ResultSet resultSet = statement.executeQuery("SELECT  * from users");
-            while (resultSet.next()) {
-                User newUser = new User(resultSet.getString(2), resultSet.getString(3), (byte) resultSet.getInt(4));
-                newUser.setId((long) resultSet.getInt(1));
-                list.add(newUser);
-            }
-            return list;
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return null;
+        String sql = (String.format("DELETE FROM users WHERE id=%s;", id));
+        runSql(sql);
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DELETE FROM users;");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        String sql = "DELETE FROM users;";
+        runSql(sql);
+    }
+
+
+    public List<User> getAllUsers() {
+        Connection connection = null;
+        Statement statement  = null;
+        ResultSet resultSet = null;
+        String sql = "SELECT * FROM users";
+        List<User> list = new ArrayList<>();
+        try {
+            connection = util.getConnection();
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            while(resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastname"));
+                user.setAge(resultSet.getByte("age"));
+                list.add(user);
+            }
+            connection.commit();
+            System.out.println("List of Users created");
+        } catch (SQLException exception) {
+            try{
+                if(connection != null){
+                    connection.rollback();
+                }
+            } catch (SQLException exceptionRollback){
+                exceptionRollback.printStackTrace();
+            }
+            exception.printStackTrace();
+        } finally {
+            try{
+                if(connection != null) {
+                    connection.close();
+                }
+                if(statement != null) {
+                    statement.close();
+                }
+                if(resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException exceptionFinally) {
+                exceptionFinally.printStackTrace();
+            }
+        }
+        return list;
+    }
+
+    private void runSql(String sql) {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = util.getConnection();
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            connection.commit();
+        } catch (SQLException exception) {
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException exceptionRollback) {
+                exceptionRollback.printStackTrace();
+            }
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException exceptionFinally) {
+                exceptionFinally.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=UTC&useSSL=false";
+    private static final String USERNAME = "nazee";
+    private static final String PASSWORD = "Antanta22!";
+
+    public Connection getConnection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
 }
